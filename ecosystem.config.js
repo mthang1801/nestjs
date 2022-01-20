217585c5213939d58bf35d16791b8603b6c1c262
kahovn <kahovn@gmail.com>
2022-01-20 19:44:42 +0700

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
   apps: [
     {
 		name: 'ddvecom-service',
-		script: './src/main.ts',
+		//script: './src/main.ts',
+		script: './dist/src/main.js',
+		node_args: '-r ts-node/register -r tsconfig-paths/register',
 		instances: 2,
 		autorestart: true,
 		exec_mode: 'cluster',
